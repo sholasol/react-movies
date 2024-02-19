f398927ae160f8914cd9a36f936c10a3ae2324ba
Solomon <sholasolomon@yahoo.com>
2024-02-19 18:11:15 +0000

    Complete sidebar items

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,56 +1,138 @@
-import React from 'react'
-import HomeIcon from '../icons/home-icon'
-import MovieIcon from '../icons/movie-icon'
-import TvSeriesIcon from '../icons/series-icon'
-import BookmarkIcon from '../icons/bookmark-icon'
-import { useLocation } from 'react-router-dom'
-import { Box } from '@mui/material'
+import { Link, useLocation } from "react-router-dom";
+import { Box, Hidden, Typography } from "@mui/material";
+import homeIcon from "../../assets/icons/icon-nav-home.svg";
+import movieIcon from "../../assets/icons/icon-nav-movies.svg";
+import tvSeriesIcon from "../../assets/icons/icon-nav-tv-series.svg";
+import bookmarkIcon from "../../assets/icons/icon-nav-bookmark.svg";
 
 const navLinks = [
-    {
-        name: "Home",
-        icon: HomeIcon,
-        link: "/",
-    },
-    {
-        name: "Movies",
-        icon: MovieIcon,
-        link: "/movies",
-    },
-    {
-        name: "Tv Series",
-        icon: TvSeriesIcon,
-        link: "/tv-series",
-    },
-    {
-        name: "Bookmarks",
-        icon: BookmarkIcon,
-        link: "/bookmarks",
-    }
-]
+  {
+    name: "Home",
+    icon: homeIcon,
+    link: "/home",
+  },
+  {
+    name: "Login",
+    icon: homeIcon,
+    link: "/",
+  },
+  {
+    name: "Movies",
+    icon: movieIcon,
+    link: "/movies",
+  },
+  {
+    name: "TV Series",
+    icon: tvSeriesIcon,
+    link: "/tv-series",
+  },
+  {
+    name: "Bookmarks",
+    icon: bookmarkIcon,
+    link: "/bookmarks",
+  },
+];
 
 const Sidebar = () => {
-    const {pathname} = useLocation();
+  const { pathname } = useLocation();
+
   return (
-    <Box sx = {{
+    <Box
+      sx={{
         backgroundColor: "#161d2f",
         padding: 2,
         borderRadius: 2,
         display: "flex",
         flexDirection: {
-            xs: "row",
-            lg: "column"
+          xs: "row",
+          lg: "column",
         },
         alignItems: "center",
         justifyContent: "space-between",
         width: {
-            sm: "100%",
-            lg: 200
+          sm: "100%",
+          lg: 200,
         },
-    }}>
-        
-    </Box>
-  )
-}
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column",
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            lg: "start",
+          },
+          width: "100%",
+        }}
+      >
+        <Hidden smDown>
+          <Typography
+            variant="h5"
+            component="h1"
+            my={2}
+            fontWeight={400}
+            fontSize={18}
+          >
+            PikaShowApp
+          </Typography>
+        </Hidden>
 
-export default Sidebar
+        <Box
+          sx={{
+            py: {
+              xs: "0px",
+              lg: "16px",
+            },
+            display: "flex",
+            flexDirection: {
+              xs: "row",
+              lg: "column",
+            },
+            gap: 4,
+          }}
+        >
+          {navLinks.map((item) => (
+            <Link
+              key={item.name}
+              to={item.link}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                <img
+                  src={item.icon}
+                  alt={item.name}
+                  style={{
+                    width: "18px",
+                    filter: `${
+                      pathname === item.link
+                        ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)"
+                        : "invert(84%)"
+                    }`,
+                  }}
+                />
+                <Hidden mdDown>
+                  <Typography>{item.name}</Typography>
+                </Hidden>
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
