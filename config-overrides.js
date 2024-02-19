0f60fab9ba2b0b85129dcf0df6b9ccdbe4b01c12
Solomon <sholasolomon@yahoo.com>
2024-02-19 23:04:06 +0000

    Movie List and trending list

--- config-overrides.js
+++ config-overrides.js
@@ -1,2 +1,4 @@
 const { addBabelPlugins, override } = require('customize-cra');
-module.exports = override(...addBabelPlugins('@mightymeld/runtime/babel-plugin-mightymeld'));
+module.exports = override(
+    ...addBabelPlugins('@mightymeld/runtime/babel-plugin-mightymeld')
+);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { SetStateAction, useState } from 'react'
 import Layout from '../../Layout'
+import { Box, InputAdornment, InputBase, Paper } from '@mui/material'
+import SearchIcon from "../../assets/icons/icon-search.svg";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+
+
+  const handleSearch = (e: {target: {value: SetStateAction<string>}}) => {
+
+  }
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      <Box>
+        <Paper 
+        component="form"
+        sx={{
+          display:"flex",
+          alignItems:"center",
+          borderRadius:"default",
+          p:1,
+          backgroundColor: "#10141f",
+          border:"none"
+        }}
+        >
+          <InputBase
+          placeholder="search for movies of TV series"
+          sx={{
+            ml:1,
+            flex:1,
+            color:"white",
+            border:"none"
+          }}
+          value={search}
+          onChange={handleSearch}
+          startAdornment = {
+            <InputAdornment position='start'>
+              <img 
+              src={SearchIcon}
+              alt='search icon'
+              width={20}
+              height={20}
+              />
+            </InputAdornment>
+          }
+          />
+        </Paper>
+      </Box>
+      <Box py={2} px={4}>
+          { search === "" ? (
+            <h1>some text</h1>
+          ) 
+          : (
+            <h1>some text</h1>
+          )}
+      </Box>
+    </Layout>
   )
 }
 
